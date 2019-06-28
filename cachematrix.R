7375c93d9f83205f359bba591b9787703c9d04e7
Caffles90 <x.inness@live.co.uk>
2019-06-28 20:27:00 +0100

    Adding assignment to new branch

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,22 @@
 
 ## Write a short comment describing this function
 
+## Essentially take the cache mean function written in the example and change 
+## the labels etc to make it easier to follow. Use the solve() function as 
+## detailed in the documentation.
+
 makeCacheMatrix <- function(x = matrix()) {
+      inv <- NULL
+      set <- function(y) {
+            x <<- y
+            inv <<- NULL
+      }
+      get <- function() x
+      set_inv <- function(inverse) inv <<- inverse
+      get_inv <- function() inv
+      list(set = set, get = get,
+           set_inv = set_inv,
+           get_inv = get_inv)
 
 }
 
@@ -12,4 +27,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      
+      inv <- x$get_inv()
+      if(!is.null(inv)) {
+            message("Getting cached data")
+            return(inv)
+      }
+      
+      data <- x$get()
+      inv <- solve(data, ...)
+      x$set_inv(inv)
+      inv
+      
 }
